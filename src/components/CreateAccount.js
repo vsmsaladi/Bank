6b088a86b9c3c1b8b9bc0fd6f087d75b612ddf61
Rishitha <rishithachinthareddy@gmail.com>
2024-09-03 19:54:35 -0400

    add1

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import './CreateAccount.css'
 function CreateAccount() {
     const [accountNumber, setAccountNumber] = useState('');
-    const [name, setName] = useState('');
+    const [initialBalance, setInitialBalance] = useState('');
 
     const handleSubmit = async () => {
         try {
-            await axios.post('http://localhost:8080/accounts/create', { accountNumber, name });
+            await axios.post('https://9fxmai9fu2.execute-api.us-east-1.amazonaws.com/create', {
+                accountNumber,
+                initialBalance
+            });
             alert('Account created successfully!');
         } catch (error) {
             alert('Error creating account');
@@ -18,7 +21,7 @@ function CreateAccount() {
         <div>
             <h1>Create Account</h1>
             <input type="text" value={accountNumber} onChange={(e) => setAccountNumber(e.target.value)} placeholder="Account Number" />
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+            <input type="number" value={initialBalance} onChange={(e) => setInitialBalance(e.target.value)} placeholder="Initial Balance" />
             <button onClick={handleSubmit}>Create Account</button>
         </div>
     );
